04bad2aac2817d22966464526fceba820db52df8
Yodamanking <67280243+Yodamanking@users.noreply.github.com>
2023-09-30 21:01:56 -0700

    trying index file

--- src/server/server.js
+++ src/server/server.js
@@ -7,9 +7,10 @@ const Constants = require('../shared/constants');
 const Game = require('./game');
 const webpackConfig = require('../../webpack.dev.js');
 
+var path = require(path);
 // Setup an Express server
 const app = express();
-app.get('/', function(req, res) {res.send("Hello there")})
+app.get('/', function(req, res) {res.sendFile(path.join(_dirname, "../client/html/index.html")});
 app.use(express.static('public'));
 
 if (process.env.NODE_ENV === 'development') {
